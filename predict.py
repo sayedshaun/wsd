8c74e853d9463d8aade5ca4c65dd29627e67322a
sayedshaun <sayedshaun4@gmail.com>
2025-05-26 20:20:09 +0600

    now best weight will be auto pick from given dir

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
+import os
 import torch
-import glob
 import argparse
 from dataset import WSDDataset
 from data_builder import DataBuilder
@@ -10,12 +10,13 @@ from train_utils import evaluation_fn
 
 
 def main(args: argparse.Namespace):
-    tokenizer = get_tokenizer("distilbert-base-uncased")
-    weight_paths = glob.glob(f"{args.data_dir}/*.pt")
-    assert len(weight_paths) == 1, f"Found {len(weight_paths)} models in {args.data_dir}"
-    weight_path = weight_paths[0]
-    model = WordSenseDisambiguationModel("distilbert-base-uncased", tokenizer=tokenizer).to(args.device)
-    model.load_state_dict(torch.load(weight_path, map_location=args.device))
+    tokenizer = get_tokenizer(args.model_name)
+    model = WordSenseDisambiguationModel(args.model_name, tokenizer=tokenizer).to(args.device)
+    weight_list = os.listdir(args.weight_dir)
+    path = sorted(weight_list, key=lambda x: x.split("-")[-1])[-1]
+    print("Loading model from: ", path)
+    state_dict = torch.load(os.path.join(args.weight_dir, path), map_location=args.device)
+    model.load_state_dict(state_dict)
     model.eval()
 
     ds = WSDDataset(
@@ -35,13 +36,13 @@ def main(args: argparse.Namespace):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add
     parser.add_argument("--data_dir", type=str, required=True, help="Path to data directory")
-    parser.add_argument("--pos_tag", type=str, default="VERB", help="POS tag to use")
+    parser.add_argument("--weight_dir", type=str, required=True, help="Path to model weight directory")
+    parser.add_argument("--pos_tag", type=str, default="ALL", help="POS tag to use")
     parser.add_argument("--num_sense", type=int, default=5, help="Number of sense")
-    parser.add_argument("--max_length", type=int, default=128, help="Maximum length of input")
+    parser.add_argument("--max_length", type=int, default=512, help="Maximum length of input")
     parser.add_argument("--batch_size", default=8, type=int, help="Batch size")
     parser.add_argument("--device", type=str, default=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     args = parser.parse_args()
     main(args)
-
-
