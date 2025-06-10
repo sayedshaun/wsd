a32943d1e0873bce883c1d4f339cfded3874abed
sayedshaun <sayedshaun4@gmail.com>
2025-06-10 11:02:59 +0600

    added dual architecture support

--- predict.py
+++ predict.py
@@ -1,40 +1,55 @@
 import os
 import torch
 import argparse
-from dataset import WSDDataset
+from dataset import WSDDataset, SpanDataset
 from data_builder import DataBuilder
-from model import WordSenseDisambiguationModel
-from utils import get_tokenizer
+from model import WSDModel, SpanExtractionModel
+from utils import get_tokenizer, load_model_weights
 from torch.utils.data import DataLoader
-from train_utils import evaluation_fn
+from train_utils import evaluation_fn, span_evaluation_fn
+
+
 
 
 def main(args: argparse.Namespace):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = get_tokenizer(args.model_name)
-    model = WordSenseDisambiguationModel(args.model_name, tokenizer=tokenizer).to(args.device)
-    weight_list = os.listdir(args.weight_dir)
-    path = sorted(weight_list, key=lambda x: x.split("-")[-1])[-1]
-    print("Loading model from: ", path)
-    state_dict = torch.load(os.path.join(args.weight_dir, path), map_location=args.device)
-    model.load_state_dict(state_dict)
-    model.eval()
-
-    ds = WSDDataset(
-        dataframe=DataBuilder(args.data_dir, args.pos, args.seed).to_pandas(), 
-        tokenizer=tokenizer, 
-        n_sense=args.num_sense, 
-        max_seq_length=args.max_length,
-        shuffle_senses=False
-    )
-    dataloader = DataLoader(ds, batch_size=args.batch_size, shuffle=False)
-    loss, f1, precision, recall, acc = evaluation_fn(model, dataloader, args.device)
-    print("=" * 50)
-    print("Loss: ", loss)
-    print("F1: ", f1)
-    print("Precision: ", precision)
-    print("Recall: ", recall)
-    print("Accuracy: ", acc)
-    print("=" * 50)
+    if args.architecture == "cosine_similarity":
+        model = WSDModel(args.model_name, tokenizer=tokenizer).to(device)
+        model = load_model_weights(model, args.weight_dir, device)
+        ds = WSDDataset(
+            dataframe=DataBuilder(args.data_dir, args.pos, args.seed).to_pandas(), 
+            tokenizer=tokenizer, 
+            n_sense=args.num_sense, 
+            max_seq_length=args.max_length,
+            shuffle_senses=False
+        )
+        dataloader = DataLoader(ds, batch_size=args.batch_size, shuffle=False)
+        loss, f1, precision, recall, acc = evaluation_fn(model, dataloader, device)
+        print("=" * 50)
+        print("Loss: ", loss)
+        print("F1: ", f1)
+        print("Precision: ", precision)
+        print("Recall: ", recall)
+        print("Accuracy: ", acc)
+        print("=" * 50)
+    else:
+        model = SpanExtractionModel(args.model_name, tokenizer=tokenizer).to(device)
+        model = load_model_weights(model, args.weight_dir, args.device)
+        ds = SpanDataset(
+            dataframe=DataBuilder(args.data_dir, args.pos, args.seed).to_pandas(), 
+            tokenizer=tokenizer, 
+            max_seq_length=args.max_length
+        )
+        dataloader = DataLoader(ds, batch_size=args.batch_size, shuffle=False)
+        loss, start_accu, end_accu, joint_accu = span_evaluation_fn(model, dataloader, device)
+        print("=" * 50)
+        print("Loss: ", loss)
+        print("Start Accuracy: ", start_accu)
+        print("End Accuracy: ", end_accu)
+        print("Joint Accuracy: ", joint_accu)
+        print("=" * 50)
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -45,6 +60,6 @@ if __name__ == "__main__":
     parser.add_argument("--num_sense", type=int, default=5, help="Number of sense")
     parser.add_argument("--max_length", type=int, default=128, help="Maximum length of input")
     parser.add_argument("--batch_size", default=8, type=int, help="Batch size")
-    parser.add_argument("--device", type=str, default=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    parser.add_argument("--architecture", required=True, type=str, help="Model architecture to use")
     args = parser.parse_args()
     main(args)
